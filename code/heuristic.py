1660c7f54162c8fe76199327ac9db7669e912941
hippolytej <hippolyte.jacomet@gmail.com>
2017-02-09 15:40:01 +0100

    Max filtre

--- code/heuristic.py
+++ code/heuristic.py
@@ -41,11 +41,18 @@ def rav(data):
     data['rav'] = (data['revenus_tot'] - data['charges'] - data['sum_mensualite'])/data['u_c']
     return data
 
+def max_filter(data):
+    """Filtre Max"""
+    data = data[(data.charges > 400) & (data.revenus_tot > 400)]
+    data['orientation'] = data.where(~(data.orientation == 3), 2)['orientation']
+    print('Nombre de lignes retenures %i' %data.shape[0])
+    return data
+
 def heuristic(data):
     """Prédiction de l'orientation en fonction des seuils 100 et 400 euros de reste à vivre"""
-    data['rav_pred'] = 3
-    data['rav_pred'] = data['rav_pred'].where(~(data['rav'] >= 400), 2)
-    data['rav_pred'] = data['rav_pred'].where(~(data['rav'] <= 100), 4)
+    data['rav_pred'] = 4
+    data['rav_pred'] = data['rav_pred'].where(~(data['rav'] >=145), 2)
+    #data['rav_pred'] = data['rav_pred'].where(~(data['rav'] <= 100), 4)
     return(data)
 
 #------------ Main -----------------------------
@@ -56,6 +63,7 @@ with open('../data/mapping.p', 'rb') as fp:
 
 # Calcul de l'unité de conso, du rav et de la prédiction
 data = pd.read_csv("../data/preprocessed_data.csv")
+data = max_filter(data)
 data = unite_consommation(data)
 data = rav(data)
 data = heuristic(data)
@@ -66,9 +74,9 @@ print(pd.crosstab(data['rav_pred'], data['orientation']))
 
 # Plots
 
-A = data[(data.orientation == 2) & (data.charges > 400) & (data.revenus_tot > 400)]
-B = data[(data.orientation == 3) & (data.charges > 400) & (data.revenus_tot > 400)]
-C = data[(data.orientation == 4) & (data.charges > 400) & (data.revenus_tot > 400)]
+A = data[(data.orientation == 2)]
+B = data[(data.orientation == 3)]
+C = data[(data.orientation == 4)]
 
 print(A.shape[0]+B.shape[0]+C.shape[0])
 
