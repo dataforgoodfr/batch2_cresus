dec7c137412ccfb5c558980ca8dced70f478c1af
qmoret <qmoretdero@gmail.com>
2017-02-21 15:47:05 +0100

    Tests avec le nouveau out.csv, et reformatage PEP8 du code

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -14,31 +14,35 @@ from import_data import import_data
 # ------- Local functions -------
 # -------------------------------
 
-def create_masks(data) :
+def create_masks(data):
     ''' Créée les masques qui serviront à séléctionner les variables pertinentes.
         Divise les variables budgétaires en sous parties.
         Les variables inutiles sont exclues de ces sous parties.
     '''
     budget = ['revenus', 'allocations',
-              'pensions_alim', 'revenus_FL', 'autre1', 'autre2', 'autre3', 'loyer',
-            'charges_loc_cop', 'gdf', 'electicite', 'eau', 'tel_fixe', 'tel_port',
-            'impots', 'taxe_fonciere', 'taxe_habitation', 'assurance_habitat',
-            'assurance_voiture', 'mutuelle', 'autre_assurance', 'epargne_enfant',
-            'frais_scolarite', 'transport_enfant', 'autres_charges_enfant',
-            'frais_bancaire', 'soins_recurrent', 'frais_justice', 'frais_transport',
-            'epargne', 'autres_charges', 'fioul_bois', 'internet', 'abonnement_tv',
-            'abonnement_autre', 'autre_charge', 'taxe_ordure', 'autre_impots',
-            'assurance_gav', 'assurance_prevoyance', 'assurance_scolaire',
-            'pensions_alim_payee', 'internat', 'frais_garde', 'cantine','alim_hyg_hab'
-            #,'dat_budget', 'typ'
-            ]
-    autres_infos = ['id','age', 'profession', 'logement', 'situation', 'retard_facture',
-                 'retard_pret','orientation','personne_charges'
-                  #, 'releve_bancaire' ,'transferable', 'nature',
-                  ]
+              'pensions_alim', 'revenus_FL', 'autre1', 'autre2', 'autre3',
+              'loyer', 'charges_loc_cop', 'gdf', 'electicite', 'eau',
+              'tel_fixe', 'tel_port', 'impots', 'taxe_fonciere',
+              'taxe_habitation', 'assurance_habitat', 'assurance_voiture',
+              'mutuelle', 'autre_assurance', 'epargne_enfant',
+              'frais_scolarite', 'transport_enfant', 'autres_charges_enfant',
+              'frais_bancaire', 'soins_recurrent', 'frais_justice',
+              'frais_transport', 'epargne', 'autres_charges', 'fioul_bois',
+              'internet', 'abonnement_tv', 'abonnement_autre', 'autre_charge',
+              'taxe_ordure', 'autre_impots', 'assurance_gav',
+              'assurance_prevoyance', 'assurance_scolaire',
+              'pensions_alim_payee', 'internat', 'frais_garde', 'cantine',
+              'alim_hyg_hab'
+              # ,'dat_budget', 'typ'
+              ]
+    autres_infos = ['id', 'age', 'profession', 'logement', 'situation',
+                    'retard_facture', 'retard_pret', 'orientation',
+                    'personne_charges'
+                    # , 'releve_bancaire' ,'transferable', 'nature',
+                    ]
     new_cols = ['sum_mensualite',
-              #'moy_nb_mensualite',
-              'sum_solde']
+                # 'moy_nb_mensualite',
+                'sum_solde']
     credit_detail = []
     for text in new_cols:
         credit_detail.append(["{}_{}".format(text, i) for i in range(6)])
@@ -50,18 +54,19 @@ def create_masks(data) :
     return [budget, autres_infos, new_cols, credit_detail, to_keep]
 
 
-def create_features(data, to_keep, credit_detail) :
+def create_features(data, to_keep, credit_detail):
     '''
         1. Aggrège les mensualités et soldes
         2. Aggrège les revenus et les charges
         3. Récupère les informations contenues dans 'orientation_old' si besoin
     '''
 
-    # Les None dans les colonnes du crédit correspondent à des 0 (absence de crédit)
+    # Les None dans les colonnes du crédit correspondent à des 0
+    # (absence de crédit)
     fill_with_zeros = [item for sublist in credit_detail for item in sublist]
     for col in fill_with_zeros:
-        data[col].fillna(value = 0,
-                    inplace = True)
+        data[col].fillna(value=0,
+                         inplace=True)
 
     # Aggrège les différents types de crédits
     for i, col in enumerate(['sum_mensualite', 'sum_solde']):
@@ -69,37 +74,41 @@ def create_features(data, to_keep, credit_detail) :
 
     # Aggrège les différentes catégories du budget
     data['revenus_tot'] = data.loc[:, ('revenus',
-                                      'allocations',
-                                      'pensions_alim',
-                                      'revenus_FL',
-                                      'autre1',
-                                      'autre2',
-                                      'autre3'
-                                      )].sum(1)
+                                       'allocations',
+                                       'pensions_alim',
+                                       'revenus_FL',
+                                       'autre1',
+                                       'autre2',
+                                       'autre3'
+                                       )].sum(1)
 
     # Aggrège les différents types de charges
-    which = ['loyer','charges_loc_cop','gdf','electicite','eau','tel_fixe','tel_port','impots','taxe_fonciere',
-      'taxe_habitation','assurance_habitat','assurance_voiture','mutuelle','autre_assurance',
-      'epargne_enfant','frais_scolarite','transport_enfant','autres_charges_enfant','frais_bancaire',
-      'soins_recurrent','frais_justice','frais_transport','epargne','autres_charges','fioul_bois',
-     'internet','abonnement_tv','abonnement_autre','autre_charge','taxe_ordure','autre_impots',
-      'assurance_gav','assurance_prevoyance','assurance_scolaire','pensions_alim_payee',
-      'internat','frais_garde','cantine']
-    data['charges'] = data.loc[:, which].sum(1, skipna = True)
+    which = ['loyer', 'charges_loc_cop', 'gdf', 'electicite', 'eau', 'tel_fixe'
+             'tel_port', 'impots', 'taxe_fonciere', 'taxe_habitation',
+             'assurance_habitat', 'assurance_voiture', 'mutuelle',
+             'autre_assurance', 'epargne_enfant', 'frais_scolarite',
+             'transport_enfant', 'autres_charges_enfant', 'frais_bancaire',
+             'soins_recurrent', 'frais_justice', 'frais_transport', 'epargne',
+             'autres_charges', 'fioul_bois', 'internet', 'abonnement_tv',
+             'abonnement_autre', 'autre_charge', 'taxe_ordure', 'autre_impots',
+             'assurance_gav', 'assurance_prevoyance', 'assurance_scolaire',
+             'pensions_alim_payee', 'internat', 'frais_garde', 'cantine']
+    data['charges'] = data.loc[:, which].sum(1, skipna=True)
 
     to_keep = to_keep + ['charges', 'revenus_tot']
 
-    # Récupère les orientations contenues dans 'orientation_old' si (orientation in [0,1])
-    d = {'surendettement'   : 4 ,
-        'accompagnement'    : 2,
-        'mediation'         : 3,
-        'microcredit'       : 5,
-        'Microcredit'       : 5}
-    data.orientation_old = data.orientation_old.apply(lambda x : d.get(x,0))
+    # Récupère les orientations contenues dans 'orientation_old' si
+    # (orientation in [0,1])
+    d = {'surendettement': 4,
+         'accompagnement': 2,
+         'mediation': 3,
+         'microcredit': 5,
+         'Microcredit': 5}
+    data.orientation_old = data.orientation_old.apply(lambda x: d.get(x, 0))
 
     data.ix[(data.orientation_old > 1)
-          & (data.orientation<=1), 'orientation'] =  data.ix[(data.orientation_old > 1)
-                                                          & (data.orientation<=1), 'orientation_old']
+            & (data.orientation <= 1), 'orientation'] = data.ix[(data.orientation_old > 1)
+                                                                & (data.orientation <= 1), 'orientation_old']
     print("\ncreate_features ---------------------------------------\n")
     return [data, to_keep]
 
@@ -114,22 +123,28 @@ def filter_data(data):
     print("\nfilter_data -------------------------------------------")
     n_tot = data.shape[0]
 
-    orientation_check = data[(data.orientation < 2)|(data.orientation > 4)].shape[0]
-    print("Orientations : %i valeurs autres que 2, 3 ou 4, soit %.2f%% du jeu." %(orientation_check, 100*orientation_check/n_tot))
+    orientation_check = data[(data.orientation < 2) |
+                             (data.orientation > 4)].shape[0]
+    print("Orientations : %i valeurs autres que 2, 3 ou 4, soit %.2f%% du jeu." % (
+        orientation_check, 100 * orientation_check / n_tot))
 
-    charges_check = data.loc[data.charges<=0,:].shape[0]
-    print("Charges : %i valeurs négatives ou nulles soit %.2f%% du jeu." %(charges_check, 100*charges_check/n_tot))
+    charges_check = data.loc[data.charges <= 0, :].shape[0]
+    print("Charges : %i valeurs négatives ou nulles soit %.2f%% du jeu." %
+          (charges_check, 100 * charges_check / n_tot))
 
     revenus_check = data[data.revenus_tot <= 0].shape[0]
-    print("Revenus : %i valeurs non retenues soit %.2f%% du jeu." %(revenus_check, 100*revenus_check/n_tot))
+    print("Revenus : %i valeurs non retenues soit %.2f%% du jeu." %
+          (revenus_check, 100 * revenus_check / n_tot))
 
-    data = data[(data.orientation > 1)&(data.orientation < 5)]
-    data = data.loc[data.charges>0,:]
-    data=data[data.revenus_tot.notnull()] # Elimine ceux pour lesquels on a pas de budget
+    data = data[(data.orientation > 1) & (data.orientation < 5)]
+    data = data.loc[data.charges > 0, :]
+    # Elimine ceux pour lesquels on a pas de budget
+    data = data[data.revenus_tot.notnull()]
 
     print("Nombre de dossiers par plateforme d'origine après filtrage :")
     for e in ["CRESUS", "social", "bancaire"]:
-        print('{:>15} | {:3.0f}'.format(e,data[data.plateforme==e].shape[0]))
+        print('{:>15} | {:3.0f}'.format(
+            e, data[data.plateforme == e].shape[0]))
     print("\n")
     return data
 
@@ -143,12 +158,13 @@ def encode_categ(data):
         if dtype == 'object':
             data[col] = data[col].apply(lambda s: str(s))
             # Replace 0 and NaNs with unique label : 'None'
-            data[col] = data[col].where(~data[col].isin(['0','nan']), 'None')
+            data[col] = data[col].where(~data[col].isin(['0', 'nan']), 'None')
             data[col] = le.fit_transform(data[col])
-            mapping[col]= dict(zip(le.inverse_transform(data[col].unique()), data[col].unique()))
+            mapping[col] = dict(zip(le.inverse_transform(
+                data[col].unique()), data[col].unique()))
 
     print("\nencode_categ ------------------------------------------\n")
-    return [data,mapping]
+    return [data, mapping]
 
 
 def detect_na(data, mapping):
@@ -157,23 +173,32 @@ def detect_na(data, mapping):
 
     print("detect_na ---------------------------------------------")
 
-    mens_check = data[(data.sum_mensualite <= 0) | (data.sum_mensualite > 8000)].shape[0]
-    print("Mensualiés : %i valeurs aberrantes soit %.2f%% du jeu. " %(mens_check, 100*mens_check/n_tot))
+    mens_check = data[(data.sum_mensualite <= 0) | (
+        data.sum_mensualite > 80000)].shape[0]
+    print("Mensualiés : %i valeurs aberrantes soit %.2f%% du jeu. " %
+          (mens_check, 100 * mens_check / n_tot))
 
-    age_check = data[(data.age<18) | (data.age>90)].shape[0]
-    print("Age : %i valeurs aberrantes soit %.2f%% du jeu. " %(age_check, 100*age_check/n_tot))
+    age_check = data[(data.age < 18) | (data.age > 90)].shape[0]
+    print("Age : %i valeurs aberrantes soit %.2f%% du jeu. " %
+          (age_check, 100 * age_check / n_tot))
 
-    pac_check = data.personne_charges[(data.personne_charges<0) | (data.personne_charges>10)].shape[0]
-    print("Personnes à charge : %i valeurs aberrantes soit %.2f%% du jeu. \n" %(pac_check, 100*pac_check/n_tot))
+    pac_check = data.personne_charges[
+        (data.personne_charges < 0) | (data.personne_charges > 10)].shape[0]
+    print("Personnes à charge : %i valeurs aberrantes soit %.2f%% du jeu. \n" %
+          (pac_check, 100 * pac_check / n_tot))
 
-    data['sum_mensualite'] = data['sum_mensualite'].where(((data.sum_mensualite > 0) & (data.sum_mensualite <= 8000)), None)
-    data['age'] = data['age'].where(((data.age>=18) & (data.age<=90)), None)
-    data['personne_charges'] = data['personne_charges'].where(((data.personne_charges>=0) & (data.personne_charges<=10)), None)
+    data['sum_mensualite'] = data['sum_mensualite'].where(
+        ((data.sum_mensualite > 0) & (data.sum_mensualite <= 80000)), None)
+    data['age'] = data['age'].where(
+        ((data.age >= 18) & (data.age <= 90)), None)
+    data['personne_charges'] = data['personne_charges'].where(
+        ((data.personne_charges >= 0) & (data.personne_charges <= 10)), None)
 
     locataire = mapping['logement']['locataire']
     for col in ['loyer', 'gdf', 'electicite', 'eau', 'assurance_habitat']:
         regle = (data.logement == locataire) & (data[col] == 0)
-        print("%s : %i valeurs sont effacées et recomplétées" %(col,data[regle][col].shape[0]))
+        print("%s : %i valeurs sont effacées et recomplétées" %
+              (col, data[regle][col].shape[0]))
         data[col] = data[col].where(~regle, None)
 
     return data
@@ -185,36 +210,40 @@ def fill_na(data):
 
     print("\nfill_data ---------------------------------------------\n")
     n_neighbors = 5
-    sparse_cols = ['sum_mensualite', 'age', 'personne_charges', 'loyer', 'gdf', 'electicite', 'eau', 'assurance_habitat']
+    sparse_cols = ['sum_mensualite', 'age', 'personne_charges',
+                   'loyer', 'gdf', 'electicite', 'eau', 'assurance_habitat']
     for col in sparse_cols:
-        if data[col].isnull().sum()>0:
-            print("%s : %.2f%% de valeurs manquantes" %(col,100*data[col].isnull().sum()/data.shape[0]))
+        if data[col].isnull().sum() > 0:
+            print("%s : %.2f%% de valeurs manquantes" %
+                  (col, 100 * data[col].isnull().sum() / data.shape[0]))
             knn = neighbors.KNeighborsRegressor(n_neighbors)
             data_temp = data.loc[~data[col].isnull(), :]
             mask = ~data.columns.isin(sparse_cols + ['id'])
             X = data_temp.loc[:, mask]
             null_index = data[col].isnull()
-            y_ = knn.fit(X, data_temp[col]).predict(data.loc[null_index,mask])
-            data.loc[null_index,col] = y_
+            y_ = knn.fit(X, data_temp[col]).predict(data.loc[null_index, mask])
+            data.loc[null_index, col] = y_
             data[col] = data[col].astype(float)
 
     return data
 
+
 def max_filter(data):
     """Filtre Max"""
     data = data[(data.charges > 400) & (data.revenus_tot > 400)]
-    data['orientation'] = data.where(~(data.orientation == 3), 2)['orientation']
-    print('Nombre de lignes retenures %i' %data.shape[0])
+    # data['orientation'] = data.where(
+    #     ~(data.orientation == 3), 2)['orientation']
+    print('Nombre de lignes retenures %i' % data.shape[0])
     return data
 
 
 # ------ Main ------
 
-data = import_data(folder = '../data')
+data = import_data(folder='../data')
 [budget, autres_infos, new_cols, credit_detail, to_keep] = create_masks(data)
 [data, to_keep] = create_features(data, to_keep, credit_detail)
 data = filter_data(data)
-data = data.loc[:,to_keep]
+data = data.loc[:, to_keep]
 [data, mapping] = encode_categ(data)
 data = detect_na(data, mapping)
 data = fill_na(data)
@@ -224,7 +253,7 @@ print("\n\nNombre final d'observations: {}".format(len(data)))
 print("Nombre final de colonnes: {}\n".format(data.shape[1]))
 
 # Sauvegarde des données préprocessées
-data.to_csv("../data/preprocessed_data.csv", index = False)
+data.to_csv("../data/preprocessed_data.csv", index=False)
 
 # Sauvegarde du mapper
 with open('../data/mapping.p', 'wb') as fp:
--- code/training.py
+++ code/training.py
@@ -2,20 +2,21 @@
 
 import pandas as pd
 import numpy as np
-from pprint import pprint
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.cross_validation import train_test_split
-from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import StratifiedKFold
 
 import xgboost as xgb
+
+data = 0
+data = 0
 
 data = pd.read_csv("../data/preprocessed_data.csv")
 
 # Useful variables
 lab = {2.: 'Accompagnement',
-       #3.: 'Mediation',
+       3.: 'Mediation',
        4.: 'Surendettement'}
 
 
@@ -23,7 +24,7 @@ lab = {2.: 'Accompagnement',
 mask = ~data.columns.isin(['orientation', 'id'])
 X = data.loc[:, mask]
 y = data.orientation
-Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, random_state = 10)
+Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, random_state=10)
 
 # --- Classification ----
 RFC = True
@@ -34,17 +35,18 @@ XGB = ~RFC
 
 
 # Random Forest model
-rfc = RandomForestClassifier(n_estimators = 200)
+rfc = RandomForestClassifier(n_estimators=200)
 feat_imp = dict()
 
 # Xgboost
-param = {'max_depth':6, 'eta':0.1, 'silent':1, 'objective':'multi:softmax' , 'num_class':3}
+param = {'max_depth': 6, 'eta': 0.1, 'silent': 1,
+         'objective': 'multi:softmax', 'num_class': 3}
 num_round = 20
 
 # Stratified K folds
 n_splits = 5
-skf = StratifiedKFold(n_splits, shuffle = True)
-accuracies, index  = np.zeros(n_splits), 0
+skf = StratifiedKFold(n_splits, shuffle=True)
+accuracies, index = np.zeros(n_splits), 0
 conf_mat = []
 
 for train_index, test_index in skf.split(X, y):
@@ -59,10 +61,10 @@ for train_index, test_index in skf.split(X, y):
             feat_imp[i] = feat_imp.get(i, 0) + j
 
     if XGB:
-        label_train = ytrain.map(lambda x : int(x-2))
-        label_test = ytest.map(lambda x : int(x-2))
-        dtrain = xgb.DMatrix(Xtrain, label = label_train)
-        dtest = xgb.DMatrix(Xtest, label = label_test)
+        label_train = ytrain.map(lambda x: int(x-2))
+        label_test = ytest.map(lambda x: int(x-2))
+        dtrain = xgb.DMatrix(Xtrain, label=label_train)
+        dtest = xgb.DMatrix(Xtest, label=label_test)
         bst = xgb.train(param, dtrain, num_round)
         label_pred = bst.predict(dtest)
         ypred = label_pred+2
@@ -70,40 +72,40 @@ for train_index, test_index in skf.split(X, y):
     accuracies[index] = np.mean(ypred == ytest)
 
     ytest_lab = ytest.map(lab)
-    ytest_lab.index = range(1,len(ytest_lab) + 1)
+    ytest_lab.index = range(1, len(ytest_lab) + 1)
     ytest_lab.name = 'Actual'
     ypred_lab = pd.Series(ypred, name='Predicted').map(lab)
     try:
         conf_mat = conf_mat + pd.crosstab(ytest_lab, ypred_lab)
     except:
         conf_mat = pd.crosstab(ytest_lab, ypred_lab)
-    index+=1
+    index += 1
 
-conf_mat /= n_splits
 for feat, imp in feat_imp.items():
-    feat_imp[feat] = imp/ n_splits
-feat_imp = sorted(feat_imp.items(), key = lambda x : x[1], reverse = True)[:10]
+    feat_imp[feat] = imp / n_splits
+feat_imp = sorted(feat_imp.items(), key=lambda x: x[1], reverse=True)[:10]
 print("\nCross validated accuracy: %0.2f (+/- %0.2f)\n" %
       (accuracies.mean(), accuracies.std()))
-if RFC :
+if RFC:
     print('Showing 10 biggest feature importances:')
-    for (i,j) in feat_imp:
-        print('{:>25} | {:3.2f}'.format(i,j/n_splits))
+    for (i, j) in feat_imp:
+        print('{:>25} | {:3.2f}'.format(i, j/n_splits))
 
 print('\nCross validated Confusion matrix :')
 print(conf_mat)
 
 # ------ XGBOOST ------
-#----------------------
+# ----------------------
 if (XGB & False):
     # Encoding to 1 - n_classes
-    label_train = ytrain.map(lambda x : int(x-2))
-    label_test = ytest.map(lambda x : int(x-2))
-    dtrain = xgb.DMatrix(Xtrain, label = label_train)
-    dtest = xgb.DMatrix(Xtest, label = label_test)
+    label_train = ytrain.map(lambda x: int(x-2))
+    label_test = ytest.map(lambda x: int(x-2))
+    dtrain = xgb.DMatrix(Xtrain, label=label_train)
+    dtest = xgb.DMatrix(Xtest, label=label_test)
 
     # specify parameters via map
-    param = {'max_depth':6, 'eta':0.1, 'silent':1, 'objective':'multi:softmax' , 'num_class':3}
+    param = {'max_depth': 6, 'eta': 0.1, 'silent': 1,
+             'objective': 'multi:softmax', 'num_class': 3}
     num_round = 20
     bst = xgb.train(param, dtrain, num_round)
     # make prediction
@@ -111,9 +113,9 @@ if (XGB & False):
     ypred = label_pred+2
     print('XGboost Accuracy is {}\n'.format(np.mean(ypred == ytest)))
 
-
     # Get cross validated accuracy
-    scores = xgb.cv(param, dtrain, num_round, nfold=5, metrics={'merror'}, seed = 0)
+    scores = xgb.cv(param, dtrain, num_round,
+                    nfold=5, metrics={'merror'}, seed=0)
     print("XGboost Cross validated accuracy: %0.2f (+/- %0.2f)" %
           (1-scores['test-merror-mean'][num_round-1],
            scores['test-merror-std'][num_round-1]))
